67897bc81ad45f786e7fa794bcd9b37b374b1868
Vadim Novash <novash@tut.by>
2017-06-23 15:22:08 +0300

    localstorage added

--- markup/components/order-form/floatingFormInput.js
+++ markup/components/order-form/floatingFormInput.js
@@ -12,3 +12,20 @@ $('.registration__form-group input').on({
         }
     }
 });
+let name = document.querySelector('#username');
+let phone = document.querySelector('#phone');
+let checkNameInStorage = localStorage.getItem('name');
+let checkPhoneInStorage = localStorage.getItem('phone');
+if (checkNameInStorage) {
+    name.value = checkNameInStorage;
+    name.parentNode.childNodes[1].classList.add('field-active');
+}
+if (checkPhoneInStorage) {
+    phone.value = checkPhoneInStorage;
+    phone.parentNode.childNodes[1].classList.add('field-active');
+}
+const form = document.querySelector('.registration');
+form.addEventListener('submit', function () {
+    localStorage.setItem('name', name.value);
+    localStorage.setItem('phone', phone.value);
+});
