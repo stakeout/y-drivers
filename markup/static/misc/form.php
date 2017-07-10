5ece5ceffe1ffef0ffe7c7359a72271e78d0995d
Vadim Novash <novash@tut.by>
2017-07-10 18:36:15 +0300

    sms send service include

--- markup/static/misc/form.php
+++ markup/static/misc/form.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'sms.ru.php';
 // define variables and set to empty values
 $name = $phone =  '';
 function test_input($data) {
@@ -44,10 +45,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $message .= "</body></html>";
   mail($to, $subject, $message, $headers);
   $json['error'] = 0; // oшибoк нe былo
+  send_sms($phone, $name);
 
 	echo json_encode($json); // вывoдим мaссив oтвeтa
 } else { // eсли мaссив POST нe был пeрeдaн
 	echo 'GET LOST!'; // высылaeм
+}
+
+function send_sms($phone, $name){
+$smsru = new SMSRU('E7E58EA3-1EA2-09DA-81F3-F488463DC40D'); // Ваш уникальный программный ключ, который можно получить на главной странице
+$data = new stdClass();
+$data->to = '79534157055';
+$data->text = 'Новая заявка от: ' . $name . ' тел. ' . $phone; // Текст сообщения
+// $data->from = ''; // Если у вас уже одобрен буквенный отправитель, его можно указать здесь, в противном случае будет использоваться ваш отправитель по умолчанию
+// $data->time = time() + 7*60*60; // Отложить отправку на 7 часов
+// $data->translit = 1; // Перевести все русские символы в латиницу (позволяет сэкономить на длине СМС)
+// $data->test = 1; // Позволяет выполнить запрос в тестовом режиме без реальной отправки сообщения
+// $data->partner_id = '1'; // Можно указать ваш ID партнера, если вы интегрируете код в чужую систему
+$sms = $smsru->send_one($data); // Отправка сообщения и возврат данных в переменную
+
+// if ($sms->status == "OK") { // Запрос выполнен успешно
+//     echo "Сообщение отправлено успешно. ";
+//     echo "ID сообщения: $sms->sms_id.";
+// } else {
+//     echo "Сообщение не отправлено. ";
+//     echo "Код ошибки: $sms->status_code. ";
+//     echo "Текст ошибки: $sms->status_text.";
+// }
 }
 
 ?>
